c40f727ad97e0d174585745b33adbeb5fb1474a1
SuluFox <obed.gakpa@amalitech.com>
2023-11-09 16:01:52 +0000

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                 <div class="dropdown">
                     <button class="btn btn-outline-success dropdown-toggle" type="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
-                        {{-- {{auth()->user()->name}} --}}
+                        {{auth()->user()->name}}
                     </button>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Profile</a></li>
