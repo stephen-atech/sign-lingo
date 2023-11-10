6f85d70e856893981cab8f5b5a674486e1a3fb62
SuluFox <obed.gakpa@amalitech.com>
2023-11-10 11:08:49 +0000

    modified:   resources/views/learning.blade.php

--- resources/views/learning.blade.php
+++ resources/views/learning.blade.php
@@ -1,97 +1,25 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="my-wrapper">
-      <div class="welcome-box">
-        <h3>Welcome<span style="color: rgb(52, 239, 176);">{{ $category->name }} contents</span></h3>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="my-wrapper">
+                <div class="welcome-box">
+                    <h3>Welcome<span style="color: rgb(52, 239, 176);">{{ $category->name }} contents</span></h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="container cards" style="background-color: rgba(0, 0, 0, 0.5);">
+            @foreach ($category->contents as $content)
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ route('storage.content.show', ['filename' => $content->image_url]) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3>{{ $content->name }}</h3>
+                        <p class="card-text">{{ $content->description }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
     </div>
-  </div>
-
-  <div class="container cards" style="background-color: rgba(0, 0, 0, 0.5);">
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>Title</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-
-    
-  </div>
-
-</div>
 @endsection
